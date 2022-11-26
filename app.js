1602f7286425ad902a775cfdd0cc2d278b28ca5c
elhamalizadeh <alizade4591@gmail.com>
2022-11-26 22:31:20 +0330

    Create a mix array of the word

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-var message = document.querySelector("message");
+const message = document.querySelector(".message");
 const guess = document.querySelector("input");
 const button = document.querySelector("button");
 const ArrayList = ["ali", "mahdi", "sajede", "sara", "parham", "mania"];
@@ -19,13 +19,27 @@ function createWord() {
   let randonNum = Math.floor(Math.random() * ArrayList.length);
   let tempWord = ArrayList[randonNum];
   console.log(tempWord);
-console.log(tempWord.split(""));
   let rand = randomArray(tempWord.split(""));
   console.log(rand);
+  console.log(rand.join(" "));
+  message.innerHTML = rand.join(" ");
+   return tempWord;
 }
 
 function randomArray(arr){
-    for( i=arr.length-1;i>0;i--){
-        console.log(arr[i]);
-    }
+    for( let i = arr.length-1 ;i>0 ;i--){
+        let temp = arr[i];
+        console.log(temp);
+        // تا حالا تونستیم اعضای آرایه رو بصورت برعکس نشون بدیم
+
+        // حالا برای هر عضو این آرایه یک عدد رندوم بدست میاریم و اون عضو رو با عضو آرایه عوض میکنیم
+        let j = Math.floor(Math.random() * (i+1));
+        console.log(j);
+        arr[i] = arr[j];
+        arr[j] = temp;
+    };
+    return arr;
 }
+
+
+//-- to be continue...
